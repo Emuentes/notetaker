0c85a1b113e0015468ded4bce8c397c15f6e2709
Muentes, Edgar <EdgarJMuentes@gmail.com>
2015-09-13 22:03:51 -0400

    feat ( react-practice: Note-taker ) -- end of video 5
    Using the NotesList sub-component for listing notes within the notes component.
    
    Created the child elements using map on the array of notes.
    
    Learned about componentDidMount, componentWillMount, and unbinding.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,15 +3,25 @@ var Router = require('react-router');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactFire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
-  mixins: [Router.State],
+  mixins: [Router.State, ReactFireMixin],
   getInitialState: function(){
     return {
       notes: ['note1', 'note2'],
       bio: {name: 'Edgar'},
       repos: [1,2,3]
     }
+  },
+  componentDidMount: function(){
+    this.ref = new Firebase('https://sizzling-heat-7707.firebaseio.com/');
+    var childRef = this.ref.child(this.getParams().username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount:function(){
+    this.unbind('notes');
   },
   render: function(){
     var username = this.getParams().username;

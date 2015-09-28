ed1fe17d4539e3bb5db24a89ec7c5268c190d1e2
Muentes, Edgar <EdgarJMuentes@gmail.com>
2015-09-27 23:40:55 -0400

    Refactor the notes components to use ES2015 imports & classes
    The handleSubmit function can be inside the React.Component declaration but the propTypes object cannot.
    
    The propTypes object has to be defined as a property of the AddNote class after it's created.

--- app/components/Notes/AddNote.js
+++ app/components/Notes/AddNote.js
@@ -1,28 +1,30 @@
-var React = require('react');
+import React from 'react';
 
-var AddNote = React.createClass({
-  propTypes:{
-    username: React.PropTypes.string.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  handleSubmit: function(){
+class AddNote extends React.Component{
+  handleSubmit(){
     // store value of text field
     var newNote = this.refs.note.getDOMNode().value;
     // clear text field
     this.refs.note.getDOMNode().value = '';
     // call addNote function & pass it newNote
     this.props.addNote(newNote);
-  },
-  render: function(){
+  }
+
+  render(){
     return (
       <div className="input-group">
         <input type="text" className="form-control" ref="note" placeholder="Add New Note" />
         <span className="input-group-btn">
-          <button className="btn btn-default" type="button" onClick={this.handleSubmit}> Submit </button>
+          <button className="btn btn-default" type="button" onClick={ this.handleSubmit.bind(this) }> Submit </button>
         </span>
       </div>
     )
   }
-});
+};
 
-module.exports = AddNote;
+AddNote.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  addNote: React.PropTypes.func.isRequired
+};
+
+export default AddNote;

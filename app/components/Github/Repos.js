23713e845462fa2d2c5cd4655370d79fba8b9b5f
Muentes, Edgar <EdgarJMuentes@gmail.com>
2015-10-02 09:28:42 -0400

    final ES6 refactoring

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 class Repos extends React.Component{
-
   render(){
-
-    var repos = this.props.repos.map( (repo, index) => {
-      return(
+    var repos = this.props.repos.map((repo, index) => {
+      return (
         <li className="list-group-item" key={index}>
           { repo.html_url && <h4><a href={repo.html_url}> {repo.name} </a></h4> }
           { repo.description && <p> {repo.description} </p> }
